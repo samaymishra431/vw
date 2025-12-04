1dfb2dfbb4d568bf873f44415def7b10c7572a2b
samaymishra431 <mishraaniket431@gmail.com>
2025-12-04 22:14:44 +0530

    first-comit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -14,5 +15,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/vw/",  
+  base: "/vw/",
 }));
